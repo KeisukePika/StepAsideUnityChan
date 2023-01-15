4ac38991de90bc29027b69844a5c6d19fcefbde9
KeisukePika <118828435+KeisukePika@users.noreply.github.com>
2023-01-15 15:56:07 +0900

    Initial commit

---
+++ Assets/Scripts/PrefabDestroyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabDestroyController : MonoBehaviour
+{
+    
+    //カメラオブジェクト
+    private GameObject maincamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.maincamera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.z <= maincamera.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
